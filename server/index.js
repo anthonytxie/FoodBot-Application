58699ffd00728caefd5f82b7296a86eb03572716
anthonytxie <anthony.xie@vidyard.com>
2017-05-14 18:36:52 -0400

    fixed port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import app from './app'
 import config from './../config/config'
 import mongooseConnection from './../db/mongoose.js'
 
-const port = process.env.port
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
