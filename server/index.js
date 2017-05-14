3b774bc49f5127ce07993f88af9a182cf4c19c18
anthonytxie <anthony.xie@vidyard.com>
2017-05-14 18:35:20 -0400

    added mongolab

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import app from './app'
 import config from './../config/config'
 import mongooseConnection from './../db/mongoose.js'
 
-app.listen(3000, () => {
-  console.log('listening on port 300')
+const port = process.env.port
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
 })
