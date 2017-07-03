e77923ebdfbaa3087e0fc287c6013b7a1d7b8207
anthonytxie <anthony.xie@vidyard.com>
2017-07-03 16:38:15 -0400

    fixed name

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const Drink = require('./schemas/Drink');
 const Fry = require('./schemas/Fry');
 const Milkshake = require('./schemas/Milkshake');
 const Order = require('./schemas/Order');
-const Session = require('./schemas/session');
+const Session = require('./schemas/Session');
 
 
 module.exports = {Burger, Drink, Fry, Milkshake, Order, Session};
