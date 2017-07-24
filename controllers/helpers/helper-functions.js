74d593b5bf80005ca1abbc89e4fe868b13fc652f
Anthony <anthonytxie@gmail.com>
2017-07-24 19:00:51 -0400

    deleted controllers

--- controllers/helpers/helper-functions.js
+++
@@ -1,9 +0,0 @@
-function promiseHelper (req, res, operation) {
-  operation
-    .then((success) => {
-      res.send(success);
-    }).catch((err) => res.send(err));
-};
-
-
-module.exports =  {promiseHelper};
