4e0ac3c94bc2214f636b4ce544ee517587502ed3
Anthony <anthonytxie@gmail.com>
2017-07-26 00:10:16 -0400

    fixed burgerMenu

--- messenger-api-helpers/messages/menu.js
+++ messenger-api-helpers/messages/menu.js
@@ -26,93 +26,93 @@ const menuMessage = {
 };
 
 const burgerMenuMessage = {
-    type: "template",
-    payload: {
-        template_type: "list",
-        top_element_style: "compact"
-    },
-
-    elements: [
-        {
-            title: "Top Bun",
-            image_url: "./images/burgers/top-bun.jpg",
-            subtitle:
-                "Two beef patties, fancy sauce, cheese, pickles, chopped lettuce, diced onion, served on a three part bun.",
+    attachment: {
+        type: "template",
+        payload: {
+            template_type: "list",
+            top_element_style: "compact",
+            elements: [
+                {
+                    title: "Top Bun",
+                    image_url: "./images/burgers/top-bun.jpg",
+                    subtitle:
+                        "Two beef patties, fancy sauce, cheese, pickles, chopped lettuce, diced onion, served on a three part bun.",
+                    buttons: [
+                        {
+                            type: "postback",
+                            title: "Order Burger",
+                            payload: JSON.stringify({
+                                type: "order-item",
+                                data: {
+                                    foodType: "burger"
+                                }
+                            })
+                        }
+                    ]
+                },
+                {
+                    title: "The Richmond",
+                    image_url: "./images/burgers/the-richmond.jpg",
+                    subtitle:
+                        "Double cheese burger with bacon & caramelized onions.",
+                    buttons: [
+                        {
+                            type: "postback",
+                            title: "Order Burger",
+                            payload: JSON.stringify({
+                                type: "order-item",
+                                data: {
+                                    foodType: "burger"
+                                }
+                            })
+                        }
+                    ]
+                },
+                {
+                    title: "Breakfast At Tiffany's",
+                    image_url: "./images/burgers/breakfast-at-tiffanys.jpg",
+                    subtitle:
+                        "Double cheese burger with bacon, caramelized onions & a friend egg.",
+                    buttons: [
+                        {
+                            type: "postback",
+                            title: "Order Burger",
+                            payload: JSON.stringify({
+                                type: "order-item",
+                                data: {
+                                    foodType: "burger"
+                                }
+                            })
+                        }
+                    ]
+                },
+                {
+                    title: "Varsity Blues",
+                    image_url: "./images/burgers/varsity-blues.jpg",
+                    subtitle: "Two beef patties, bacon, & blue cheese.",
+                    buttons: [
+                        {
+                            type: "postback",
+                            title: "Order Burger",
+                            payload: JSON.stringify({
+                                type: "order-item",
+                                data: {
+                                    foodType: "burger"
+                                }
+                            })
+                        }
+                    ]
+                }
+            ],
             buttons: [
                 {
+                    title: "View More",
                     type: "postback",
-                    title: "Order Burger",
-                    payload: JSON.stringify({
-                        type: "order-item",
-                        data: {
-                            foodType: "burger"
-                        }
-                    })
+                    payload: "payload"
                 }
             ]
-        },
-        {
-            title: "The Richmond",
-            image_url: "./images/burgers/the-richmond.jpg",
-            subtitle:
-                "Double cheese burger with bacon & caramelized onions.",
-            buttons: [
-                {
-                    type: "postback",
-                    title: "Order Burger",
-                    payload: JSON.stringify({
-                        type: "order-item",
-                        data: {
-                            foodType: "burger"
-                        }
-                    })
-                }
-            ]
-        },
-        {
-            title: "Breakfast At Tiffany's",
-            image_url: "./images/burgers/breakfast-at-tiffanys.jpg",
-            subtitle:
-                "Double cheese burger with bacon, caramelized onions & a friend egg.",
-            buttons: [
-                {
-                    type: "postback",
-                    title: "Order Burger",
-                    payload: JSON.stringify({
-                        type: "order-item",
-                        data: {
-                            foodType: "burger"
-                        }
-                    })
-                }
-            ]
-        },
-        {
-            title: "Varsity Blues",
-            image_url: "./images/burgers/varsity-blues.jpg",
-            subtitle:
-                "Two beef patties, bacon, & blue cheese.",
-            buttons: [
-                {
-                    type: "postback",
-                    title: "Order Burger",
-                    payload: JSON.stringify({
-                        type: "order-item",
-                        data: {
-                            foodType: "burger"
-                        }
-                    })
-                }
-            ]
-        },
-    ],
-    buttons: [
-        {
-            title: "View More",
-            type: "postback",
-            payload: "payload"
         }
-    ]
+    }
 };
 
 module.exports = { menuMessage, burgerMenuMessage };
