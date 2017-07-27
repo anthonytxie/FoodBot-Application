3f8e8a7d89d35357df91f054d5e297333e68d98d
Anthony <anthonytxie@gmail.com>
2017-07-26 23:42:06 -0400

    working on generic burger prompts

--- messenger-api-helpers/messages/burgerTemplate.js
+++ messenger-api-helpers/messages/burgerTemplate.js
@@ -1,9 +1,11 @@
 const { normalBurgers, specialBurgers } = require("./burgers");
 
 const findBurger = function(payloadData) {
-	return [...normalBurgers,...specialBurgers].filter((x) => {
-		return x.title === payloadData.title;
-	}).pop();
+	return [...normalBurgers, ...specialBurgers]
+		.filter(x => {
+			return x.title === payloadData.title;
+		})
+		.pop();
 };
 
 const burgerTemplate = function(payloadData) {
@@ -12,27 +14,30 @@ const burgerTemplate = function(payloadData) {
 		attachment: {
 			type: "template",
 			payload: {
-				template_type: "button",
-				text:
-					`Okay so, ${burger.title}, would you like the usual way or do you want to customize it?`,
-				buttons: [
+				template_type: "generic",
+				elements: [
 					{
-						type: "postback",
-						title: "The Usual",
-						payload: "USER_DEFINED_PAYLOAD"
-					},
-					{
-						type: "postback",
-						title: "Customize",
-						payload: "USER_DEFINED_PAYLOAD"
+						title: `Okay so, ${burger.title}, would you like the usual way or do you want to customize it?`,
+						image_url: burger.image_url,
+						subtitle: burger.subtitle,
+						buttons: [
+							{
+								type: "postback",
+								title: "The Usual",
+								payload: "USER_DEFINED_PAYLOAD"
+							},
+							{
+								type: "postback",
+								title: "Customize",
+								payload: "USER_DEFINED_PAYLOAD"
+							}
+						]
 					}
 				]
 			}
 		}
 	};
-	return attachment
+	return attachment;
 };
 
-
-
-module.exports = {burgerTemplate};
+module.exports = { burgerTemplate };
