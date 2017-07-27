249682c49c983b5fb3d80c674070a308b9445094
Anthony <anthonytxie@gmail.com>
2017-07-26 23:27:51 -0400

    working on generic burger prompts

--- messenger-api-helpers/messages/burgerTemplate.js
+++ messenger-api-helpers/messages/burgerTemplate.js
@@ -1,9 +1,8 @@
 const { normalBurgers, specialBurgers } = require("./burgers");
 
 const findBurger = function(payloadData) {
-	const {data} = JSON.parse(payloadData); 
 	return [...normalBurgers,...specialBurgers].filter((x) => {
-		return x.title === data.title;
+		return x.title === payloadData.data.title;
 	});
 };
 
