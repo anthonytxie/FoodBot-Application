f9839a6e7dd40cf85661486baa60b269eea98a33
isabelleleeson <isabelle.leeson@hotmail.com>
2017-07-30 18:24:45 -0400

    Add receipt view

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,13 +19,17 @@ routes.get('/burgercombo', (req,res) => {
   res.render('burgercombopage'); //send back pug file
 });
 
-routes.get('/receipt', (req, res) => {
-  let orderId = req.query.order;
-  orderDAO.getOrderById(orderId)
-    .then((order) => {
-      res.send(order);
-    });
+routes.get('/receipt', (req,res) => {
+  res.render('receipt');
 });
+
+// routes.get('/receipt', (req, res) => {
+//   let orderId = req.query.order;
+//   orderDAO.getOrderById(orderId)
+//     .then((order) => {
+//       res.send(order);
+//     });
+// });
 
 
 routes.get('/burgercustomize', (req,res) => {
