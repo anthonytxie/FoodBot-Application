62b8c095478a23ba506cb3b426f7f70d3761fc13
anthonytxie <anthony.xie@vidyard.com>
2017-07-30 13:30:32 -0400

    session active testing

--- messenger-api-helpers/receive/receiveMessage.js
+++ messenger-api-helpers/receive/receiveMessage.js
@@ -4,7 +4,6 @@ const runner = require("./../runner");
 const handleReceiveMessage = messagingEvent => {
   const message = messagingEvent.message;
   //On Tue May 17 format of user and page ids delivered via webhooks will change from an int to a string
-  const { type, data } = JSON.parse(messagingEvent.message.quick_reply.payload);
   //On Tue May 17 format of user and page ids delivered via webhooks will change from an int to a string
   const senderId = messagingEvent.sender.id.toString();
   // runner does stuff with API.ai and webhook
@@ -17,6 +16,7 @@ const handleReceiveMessage = messagingEvent => {
   runner.isSessionActive(senderId).then(isSessionActive => {
     if (isUserCreated) {
         if (message.quick_reply) {
+          const { type, data } = JSON.parse(messagingEvent.message.quick_reply.payload);
           //assuming payload is an object that has type and data
           switch (type) {
             case "see-menu":
