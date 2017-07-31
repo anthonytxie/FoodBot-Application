a7bf9585009f3889e2d980cfa6e7cb3da3517896
Anthony <anthonytxie@gmail.com>
2017-07-31 19:46:35 -0400

    fixed receipt template

--- messenger-api-helpers/messages/receiptTemplate.js
+++ messenger-api-helpers/messages/receiptTemplate.js
@@ -3,7 +3,7 @@ const { normalBurgers, specialBurgers, findBurger } = require("./burgers");
 const receiptElements = function(order) {
   return order._items.map(x => {
     if (x.itemType === "burger") {
-      let burger = findBurger(x.name);
+      let burger = findBurger(x.itemName);
       return {
         title: burger.title,
         subtitle: burger.subtitle,
@@ -21,6 +21,65 @@ const receiptElements = function(order) {
   });
 };
 
+
+// order = {
+//   _id: "597fbf1c245369001139c528",
+//   _user: "597fbf1c245369001139c526",
+//   _session: "597fbf1c245369001139c527",
+//   __v: 0,
+//   isDelivery: false,
+//   isConfirmed: false,
+//   isCompleted: false,
+//   createdAt: "2017-07-31T23:37:00.390Z",
+//   _items: [
+//     {
+//       _id: "597fbf2c245369001139c52f",
+//       _order: "597fbf1c245369001139c528",
+//       patties: 1,
+//       itemName: "Top Bun",
+//       __v: 0,
+//       kind: "Burger",
+//       createdDate: "2017-07-31T23:37:16.764Z",
+//       standardToppings: [
+//         "standardBun",
+//         "fancySauce",
+//         "lettuce",
+//         "pickles",
+//         "onions"
+//       ],
+//       premiumToppings: ["standardCheese"],
+//       itemType: "burger",
+//       price: 9.99,
+//       id: "597fbf2c245369001139c52f"
+//     },
+//     {
+//       _id: "597fbf4e245369001139c530",
+//       itemName: "gingerAle",
+//       itemCombo: true,
+//       __v: 0,
+//       kind: "Drink",
+//       createdDate: "2017-07-31T23:37:50.956Z",
+//       itemType: "drink",
+//       price: 1.29,
+//       id: "597fbf4e245369001139c530"
+//     },
+//     {
+//       _id: "597fbf4f245369001139c531",
+//       __v: 0,
+//       kind: "Sides",
+//       createdDate: "2017-07-31T23:37:51.072Z",
+//       itemCombo: true,
+//       itemName: "smallFries",
+//       itemType: "Sides",
+//       price: 2,
+//       id: "597fbf4f245369001139c531"
+//     }
+//   ],
+//   orderPrice: "13.28",
+//   id: "597fbf1c245369001139c528"
+// };
+
+console.log(receiptElements(order))
 
 const receiptMessageTemplate = function(order) {
   const attachment = {
