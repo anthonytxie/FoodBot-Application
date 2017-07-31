c3aa764c063784a88ff88dc41047b2a9550c433b
Anthony <anthonytxie@gmail.com>
2017-07-31 19:36:05 -0400

    get all orders route

--- db/DAO/orderDAO.js
+++ db/DAO/orderDAO.js
@@ -30,9 +30,10 @@ orderDAO.getOrderById = (orderId) => {
 }
 orderDAO.getAllOrders = () => {
   return new Promise((resolve, reject) => {
-    populateOrder(Order.findOne({}))
-      .then((orders) => resolve(orders))
-      .catch((err) => reject(err))
+    Order.find({}).populate("_items").exec(function(err, orders) {
+      if (err) return handleError(err);
+      resolve(orders)
+    });
   });
 };
 
