03018be5b9c2806d01eb9037d2f5aacaa9bad807
Anthony <anthonytxie@gmail.com>
2017-08-04 02:09:09 -0400

    set timeout length

--- messenger-api-helpers/send.js
+++ messenger-api-helpers/send.js
@@ -25,7 +25,7 @@ const sendSpecialBurgerMenu = (recipientId, data) => {
   sendMessage(recipientId, messages.specialBurgerMenuMessageOne);
   setTimeout(() => {
     sendMessage(recipientId, messages.specialBurgerMenuMessageTwo);
-  }, 500);
+  }, 400);
 };
 
 
@@ -34,10 +34,10 @@ const sendNormalBurgerMenu = (recipientId, data) => {
   sendMessage(recipientId, messages.normalBurgerMenuMessageOne);
   setTimeout(() => {
     sendMessage(recipientId, messages.normalBurgerMenuMessageTwo);
-  }, 500);
+  }, 400);
   setTimeout(() => {
     sendMessage(recipientId, messages.normalBurgerMenuMessageThree);
-  }, 500);
+  }, 800);
 };
 
 const sendDrinkMenuMessage = (recipientId, data) => {
