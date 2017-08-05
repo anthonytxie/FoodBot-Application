1012a6d4083f85a1fcfd3be9b33b03dd56fe64e4
anthonytxie <anthony.xie@vidyard.com>
2017-08-04 22:09:28 -0400

    tokens

--- messenger-api-helpers/sendApi.js
+++ messenger-api-helpers/sendApi.js
@@ -6,7 +6,7 @@ const isEmpty = require('lodash/isEmpty');
 const request =  require('request');
 
 
-const PAGE_ACCESS_TOKEN = process.env.page_token || 'EAACH5pouINQBAGRIEVUI1dg9BwjBg0b6vIiZC6HkLwLo6YhUrpvIU1xNtKvYKZAxyxt5CDYviuIdYAezlE7LNGlR7TQe1uDRIvc5zhhw3kabwHUPzBKWhV6iWXIaVCJuKIvKm7BiZCNst2fJp7dZAw77EqxGPbAFlmeZCnk2FCwZDZD';
+const PAGE_ACCESS_TOKEN = process.env.page_token;
 
 const callAPI = (endPoint, messageDataArray, queryParams = {}, retries = 0) => {
   // Error if developer forgot to specify an endpoint to send our request to
