3d9f4b1f6b4062bd2321e19065ec1ed540642447
anthonytxie <anthony.xie@vidyard.com>
2017-08-05 15:46:07 -0400

    fixed burger customize bug

--- server/routes/burger.js
+++ server/routes/burger.js
@@ -66,7 +66,6 @@ routes.post("/burger", (req, res) => {
   itemDAO
     .postBurger(burger.foodObject, burger._order)
     .then(order => {
-      console.log(order._id)
       return send.sendOrderedBurgerUpsizeMessage(senderId, order);
     })
     .catch(err => console.log(err));
