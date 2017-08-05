fb75a86292ed03ef84c4b4ccf4f809c92741e6f1
anthonytxie <anthony.xie@vidyard.com>
2017-08-05 18:47:36 -0400

    adding stripe

--- server/routes/receipt.js
+++ server/routes/receipt.js
@@ -4,6 +4,7 @@ const routes = express();
 
 //DAO
 const orderDAO = require("./../../db/DAO/orderDAO");
+const itemDAO = require("./../../db/DAO/itemDAO");
 
 //SEND FUNCTIONS
 const send = require("../../messenger-api-helpers/send");
@@ -25,6 +26,40 @@ routes.get("/orders", (req, res) => {
       res.send(orders);
     })
     .catch(err => console.log(err));
+});
+
+routes.post("/delete", (req, res) => {
+  let orderId = req.body.orderId;
+
+  if (req.body.burgerId & (req.body.sideId)) {
+    let burgerId = req.body.burgerId;
+    let drinkId = req.body.drinkId;
+    let sideId = req.body.sideId;
+    itemDAO.deleteItemById(orderId, burgerId)
+      .then(() => {
+        itemDAO.deleteItemById(orderId, drinkId);
+      })
+      .then(() => {
+        itemDAO.deleteItemById(orderId, sideId);
+      })
+      .then(() => {
+        res.status(200);
+      })
+  }
+  else if (req.body.sideId) {
+    let sideId = req.body.sideId;
+    itemDAO.deleteItemById(orderId, sideId)
+    .then(() => {
+      res.status(200);
+    })
+  }
+  else if (req.body.drinkId) {
+    let drinkId =req.body.drinkId; 
+    itemDAO.deleteItemById(orderId, drinkId)
+    .then(() => {
+      res.status(200);
+    })
+  }
 });
 
 module.exports = routes;
