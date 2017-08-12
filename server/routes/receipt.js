509c032182104f5e4d38c30d30db313e69148417
anthonytxie <anthony.xie@vidyard.com>
2017-08-12 18:36:21 -0400

    receipt route

--- server/routes/receipt.js
+++ server/routes/receipt.js
@@ -84,7 +84,6 @@ routes.post("/confirm", (req, res) => {
     token_email,
     authorized_payment
   } = req.body;
-  time = Date.parse(time);
   if (token_id) {
     let amount = parseFloat(authorized_payment);
     stripe.customers
