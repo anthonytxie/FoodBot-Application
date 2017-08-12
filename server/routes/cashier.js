d3fdd321d545bdeef2a11f07614137afd8b3aa37
anthonytxie <anthony.xie@vidyard.com>
2017-08-12 16:35:07 -0400

    wrap orders cashier view

--- server/routes/cashier.js
+++ server/routes/cashier.js
@@ -8,7 +8,7 @@ const orderDAO = require("./../../db/DAO/orderDAO");
 routes.get("/cashier", (req, res) => {
   orderDAO.showIncompleteOrders().then(orders => {
   	console.log(orders);
-    res.render("cashier.pug", orders);
+    res.render("cashier.pug", {orders});
   });
 });
 
