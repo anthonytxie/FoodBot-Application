4b0eb5d2465be53ac351e220ee8bc97772b05676
anthonytxie <anthony.xie@vidyard.com>
2017-08-12 18:44:46 -0400

    converting to date before saving

--- server/routes/receipt.js
+++ server/routes/receipt.js
@@ -84,6 +84,7 @@ routes.post("/confirm", (req, res) => {
     token_email,
     authorized_payment
   } = req.body;
+  time = Date(time);
   if (token_id) {
     let amount = parseFloat(authorized_payment);
     stripe.customers
@@ -134,3 +135,6 @@ routes.post("/confirm", (req, res) => {
 // if there is no items in the delete request, return just the order... else loop through delete everything. at the end get the order and send it
 
 module.exports = routes;
+
+
+
