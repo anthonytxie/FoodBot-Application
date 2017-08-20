c12ecda3c0ff136e211ae4e85f01a3c1ffc5095a
anthonytxie <anthony.xie@vidyard.com>
2017-08-19 21:11:35 -0400

    time fix

--- server/routes/receipt.js
+++ server/routes/receipt.js
@@ -71,7 +71,7 @@ routes.post("/confirm", (req, res) => {
     token_email,
     authorized_payment
   } = req.body;
-  time = Date(time)
+  time = new Date(time)
   let parsedDate = Date.parse(time);
   let fulfillmentDate = moment(parsedDate).format("YYYY-MM-HH-MM-SS");
   if (token_id) {
@@ -140,4 +140,8 @@ routes.post("/confirm", (req, res) => {
 // if there is no items in the delete request, return just the order... else loop through delete everything. at the end get the order and send it
 
 module.exports = routes;
+
+
+
+
 
