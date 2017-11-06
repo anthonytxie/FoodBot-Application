14122963a2103aacbefd4bdc8339cfb1231b01ee
anthonytxie <anthony.xie@vidyard.com>
2017-11-06 18:04:20 -0500

    added logging and googlemaps distance matrix

--- server/logger/logger.js
+++ server/logger/logger.js
@@ -1,4 +1,5 @@
 const winston = require("winston");
+require("winston-daily-rotate-file");
 
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, label, printf } = format;
@@ -7,34 +8,35 @@ const myFormat = printf(info => {
   return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
 });
 
+const infotransport = new winston.transports.DailyRotateFile({
+  filename: "/Users/axie/desktop/foodbot-application/server/logger/info.log",
+  datePattern: "yyyy-MM-dd.",
+  prepend: true,
+  level: "info"
+});
+
+const errorTransport = new winston.transports.DailyRotateFile({
+  filename: "/Users/axie/desktop/foodbot-application/server/logger/error.log",
+  datePattern: "yyyy-MM-dd.",
+  prepend: true,
+  level: "error"
+});
 
 const logger = winston.createLogger({
   level: "info",
-  format: combine(
-    label({ label: 'right meow!' }),
-    timestamp(),
-    myFormat
-  ),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({ filename: "/Users/axie/desktop/foodbot-application/server/logger/error.log", level: "error" }),
-    new winston.transports.File({ filename: "/Users/axie/desktop/foodbot-application/server/logger/info.log", level:"info" })
-  ]
+  format: combine(timestamp(), myFormat),
+  transports: [infotransport, errorTransport]
 });
-
 
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-// if (process.env.NODE_ENV !== "production") {
-//   logger.add(
-//     new winston.transports.Console({
-//       format: winston.format.simple()
-//     })
-//   );
-// }
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
 
-module.exports = {logger} ;
+module.exports = { logger };
