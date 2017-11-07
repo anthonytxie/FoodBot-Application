84abbf329c1da8cc7891d6fa463cffd92b60bdf4
anthonytxie <anthony.xie@vidyard.com>
2017-11-06 22:38:51 -0500

    correcting Production to production

--- server/logger/logger.js
+++ server/logger/logger.js
@@ -25,7 +25,7 @@ const logger = winston.createLogger({
   ]
 });
 
-if (process.env.NODE_ENV != "production") {
+if (process.env.NODE_ENV == "production") {
   logger.add(
     new winston.transports.Console({
       format: winston.format.simple()
