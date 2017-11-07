767633fb09a8dc18f0148eb6a3d898c64aea2f1b
anthonytxie <anthony.xie@vidyard.com>
2017-11-06 19:15:16 -0500

    swtiched back to winston 3

--- server/logger/logger.js
+++ server/logger/logger.js
@@ -1,27 +1,29 @@
 const winston = require("winston");
 
 const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(info => {
+  return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+});
 
 
-const logger = winston.Logger({
+const logger = winston.createLogger({
   level: "info",
+  format: combine(
+    timestamp(),
+    myFormat
+  ),
   transports: [
     //
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-    // new winston.transports.File({
-    //   filename:
-    //     "/Users/axie/desktop/foodbot-application/server/logger/error.log",
-    //   level: "error"
-    // }),
-    new winston.transports.File({
-      filename:
-        "/Users/axie/desktop/foodbot-application/server/logger/info.log",
-      level: "info"
-    })
+    new winston.transports.File({ filename: "/Users/axie/desktop/foodbot-application/server/logger/error.log", level: "error" }),
+    new winston.transports.File({ filename: "/Users/axie/desktop/foodbot-application/server/logger/info.log", level:"info" })
   ]
 });
+
 
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
@@ -34,4 +36,4 @@ const logger = winston.Logger({
 //   );
 // }
 
-module.exports = { logger };
+module.exports = {logger} ;
