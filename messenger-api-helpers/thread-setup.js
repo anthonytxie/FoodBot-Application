671ef16a02f9df4cb4ad6a0cab0231ac129b84b7
Chandler Piche <chandler.piche@gmail.com>
2017-11-13 13:07:45 -0500

    add css to help with very long item names

--- messenger-api-helpers/thread-setup.js
+++ messenger-api-helpers/thread-setup.js
@@ -33,7 +33,7 @@ curl -X POST -H "Content-Type: application/json" -d '{
 /*
 curl -X POST -H "Content-Type: application/json" -d '{
   "setting_type" : "domain_whitelisting",
-  "whitelisted_domains" : ["https://df559e6c.ngrok.io"],
+  "whitelisted_domains" : ["https://a9836c88.ngrok.io/"],
   "domain_action_type": "add"
 }' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAACAWcKsWncBAKykRZANUSEH3ah8Fk6GoW8ZBFSQ5BZC1JoQwHZAOUUVgV4q7be0gNPAXhRycAyw87L6rinDKJDtUkx8BFGMeu7uxZCCyNg2zfmlUWCAgEVIOBZA8VO8SZAoV3S9Y3WL5UcaLuYZAwiQdLZB90h95e29W9munWusYpQZDZD"
 */
